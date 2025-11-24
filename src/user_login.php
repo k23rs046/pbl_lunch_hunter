22d8e8f90b23ba360938deacfd630fbae5e99150
Rs047-el <209739223+Rs047-el@users.noreply.github.com>
2025-11-24 09:45:33 +0900

    Update user_login.php

--- src/user_login.php
+++ src/user_login.php
@@ -1,9 +1,9 @@
-<from action="?do=usr_check" method="post">
+<from action="?do=user_check" method="post">
     <table>
         <tr><td>ユーザー名：</td><td><input type="text" name="user_id"></td></tr>
         <tr><td>パスワード：</td><td><input type="text" name="user_id"></td></tr>
         <tr><td></td><td>
-            <imput type="submit" value="送信">&nbsp;<input type="reset" vale="取消">
+            <input type="submit" value="送信">&nbsp;<input type="reset" vale="取消">
         </td></tr>
     </table>
 </from>
