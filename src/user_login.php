e2c67a434e72e7f1b055f0d2cfc11f39e6ae0f0d
Inoue Satoshi <k23rs019@st.kyusan-u.ac.jp>
2025-12-01 12:17:03 +0900

    inoue

--- src/user_login.php
+++ src/user_login.php
@@ -1,9 +1,45 @@
-<form action="?do=user_check" method="post">
-    <table>
-        <tr><td>ユーザ名：</td><td><input type="text" name="user_id"></td></tr>
-        <tr><td>パスワード：</td><td><input type="password" name="user_password"></td></tr>
-        <tr><td></td><td>
-            <input type="submit" value="送信">&nbsp;<input type="reset" vale="取消">
-        </td></tr>
-    </table>
-</form>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>ユーザーログイン</title>
+    <link rel="stylesheet" href="css/login.css"> <!-- 先ほど作ったCSS -->
+</head>
+<body>
+
+<div class="login-container">
+
+    <div class="login-card">
+
+        <!-- ヘッダー -->
+        <div class="login-header">
+            <div class="login-header-spacing">
+                
+                <h1 class="login-title">ログイン</h1>
+            </div>
+        </div>
+
+        <!-- フォーム -->
+        <form class="login-form" action="?do=user_check" method="post">
+            <div class="login-field">
+                <label>ユーザ名</label>
+                <input type="text" name="user_id" class="login-input" required>
+            </div>
+
+            <div class="login-field">
+                <label>パスワード</label>
+                <input type="password" name="user_password" class="login-input" required>
+            </div>
+
+            <div class="login-button-container">
+                <button type="submit" class="login-button">送信</button>
+                <button type="reset" class="login-button" style="background-color:#dc2626;">取消</button>
+            </div>
+        </form>
+
+    </div>
+
+</div>
+
+</body>
+</html>
