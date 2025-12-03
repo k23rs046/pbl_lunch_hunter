7a1b2e354da91c7b16791ba5dadf21cd0f971bb6
Rs047-el <209739223+Rs047-el@users.noreply.github.com>
2025-12-03 17:01:14 +0900

    Update rst_save.php

--- src/rst_save.php
+++ src/rst_save.php
@@ -1,2 +1,22 @@
 <?php
-$data =[''=>'',''=>'',];
+require_once 'model.php';
+
+$holiday = array_sum($_POST['rst_holiday'] ?? []);
+
+$data =[
+     'rst_name'=> $_POST['rst_name']
+    ,'rst_address'=> $_POST['rst_address']
+    ,'start_time'=> $_POST['start_time']
+    ,'end_time'=> $_POST['end_time']
+    ,'tel_num'=> $_POST['tel_num']
+    ,'rst_holiday'=> $holiday
+    ,'rst_pay'=> null
+    ,'rst_info'=> $_POST['rst_info'] ?? null
+    ,'photo'=> $_POST['photo'] ?? null
+    ,'user_id'=> $_POST['user_id']
+    ,'discount'=> false
+];
+if(isset($_POST['rst_pay'])){
+    $pay = array_sum($_POST['rst_pay'] ?? []);
+    $data['rst_pay'] = $pay;
+}
